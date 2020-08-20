a338dc594b9e4834c1ed98fa6eeede05d2884114
Bob Gendler <boberito@mac.com>
2020-08-19 21:50:42 -0400

    Initial Commit

---
+++ MSCP/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MSCP
+//
+//  Created by Bob Gendler on 8/19/20.
+//  Copyright © 2020 Bob Gendler. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MSCP/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MSCP
+//
+//  Created by Bob Gendler on 8/19/20.
+//  Copyright © 2020 Bob Gendler. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
